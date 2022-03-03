5a75730a4a50ac40c388c1c82318d05533becd85
Robin9800 <robin.costanzo@gmail.com>
2022-03-03 14:45:34 +0100

    "Random number" added + exercise completed

--- exercise.js
+++ exercise.js
@@ -1,8 +1,11 @@
 /*chiedere all'utente il proprio NOME, COGNOME e COLORE PREFERITO*/
-const yourName = String( prompt('qual è il tuo nome?'));
+const yourName = String( prompt('Qual è il tuo nome?'));
 const surname = String( prompt('e il tuo cognome?'));
-const color = String(prompt('ok, ora dimmi il tuo colore preferito'));
+const color = String(prompt('Ok, ora dimmi il tuo colore preferito'));
 console.log(yourName);
 console.log(surname);
 console.log(color);
 
+
+
+document.getElementById('random-password').innerHTML = 'La tua password generata è ' + yourName + surname + color + Math.floor(Math.random() * 101);
