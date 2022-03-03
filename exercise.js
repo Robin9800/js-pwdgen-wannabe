feff9b7a37e1dcb111c5bd41bf37138b6ce80551
Robin9800 <robin.costanzo@gmail.com>
2022-03-03 14:22:57 +0100

    Richiesta informazioni all'utente

---
+++ exercise.js
@@ -0,0 +1,8 @@
+/*chiedere all'utente il proprio NOME, COGNOME e COLORE PREFERITO*/
+const yourName = String( prompt('qual è il tuo nome?'));
+const surname = String( prompt('e il tuo cognome?'));
+const color = String(prompt('ok, ora dimmi il tuo colore preferito'));
+console.log(yourName);
+console.log(surname);
+console.log(color);
+
